5b0905c2b34d4cc349d7617106126ba667b8e0d6
DavideBasile1810355 <64369787+DaveBasile09@users.noreply.github.com>
2021-06-14 02:53:42 +0200

    UISignUp

--- Chess-master/app/src/main/java/com/example/dipanshkhandelwal/chess/Welcome.java
+++ Chess-master/app/src/main/java/com/example/dipanshkhandelwal/chess/Welcome.java
@@ -15,10 +15,18 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         Button signInButton=(Button) findViewById(R.id.login_welcome);
+        Button signUpButton=(Button) findViewById(R.id.signup_welcome);
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SignUp.class);
                 startActivity(intent);
             }
         });
